768b75996d8dc3c9e19f4bbe3d4e6936548027ee
KihaJeong1994 <jkha7371@gmail.com>
2023-01-11 16:59:29 +0900

    dev-world basic frame

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lime,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Develop World'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,16 +29,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
+  bool _extended = true;
 
-  void _incrementCounter() {
+  void _setSelectedIndex(int value) {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = value;
+    });
+  }
+
+  void _setExtended() {
+    setState(() {
+      _extended = !_extended;
     });
   }
 
@@ -69,47 +57,83 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    final ButtonStyle style = TextButton.styleFrom(
+      foregroundColor: Theme.of(context).colorScheme.onPrimary,
+    );
+    Widget page;
+    switch (_selectedIndex) {
+      case 0:
+        page = Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                '인강 리스트 불러오기',
+              ),
+            ],
+          ),
+        );
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('No page for $_selectedIndex');
+    }
+    IconData arrowIconData;
+    arrowIconData =
+        _extended ? Icons.arrow_back_ios_new : Icons.arrow_forward_ios;
     return Scaffold(
       appBar: AppBar(
+        leading: const Icon(Icons.accessibility_new_rounded),
+        actions: <Widget>[
+          TextButton(
+            style: style,
+            onPressed: (() {}),
+            child: TextButton.icon(
+                onPressed: (() {}),
+                style: TextButton.styleFrom(foregroundColor: Colors.black),
+                icon: const Icon(Icons.login),
+                label: const Text('로그인')),
+          )
+        ],
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: false,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: LayoutBuilder(builder: (context, constraints) {
+        return Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                leading: IconButton(
+                  onPressed: (() => _setExtended()),
+                  icon: Icon(arrowIconData),
+                ),
+                extended: constraints.maxWidth >= 600 && _extended,
+                destinations: const [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.computer),
+                    label: Text('인강'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.mail),
+                    label: Text('문의'),
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onDestinationSelected: (value) => _setSelectedIndex(value),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: Container(
+                child: page,
+              ),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      }),
     );
   }
 }
