2f11d02088da06a2180e8a73aed861da7b2b4d5d
KihaJeong1994 <jkha7371@gmail.com>
2023-01-31 12:15:03 +0900

    add url launcher for hyperlink

--- lib/screens/lecture/lecture_detail.dart
+++ lib/screens/lecture/lecture_detail.dart
@@ -1,6 +1,7 @@
 import 'package:develop_world/model/lecture.dart';
 import 'package:develop_world/widgets/common/five_star_rate.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LectureDetail extends StatefulWidget {
   final String id;
@@ -22,6 +23,13 @@ class _LectureDetailState extends State<LectureDetail> {
       'https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%EC%9E%85%EB%AC%B8-%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8';
   String description =
       '스프링 입문자가 예제를 만들어가면서 스프링 웹 애플리케이션 개발 전반을 빠르게 학습할 수 있습니다.';
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(Uri.parse(url))) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -63,7 +71,10 @@ class _LectureDetailState extends State<LectureDetail> {
             Column(
               children: [
                 Text(title),
-                Text(site.name),
+                TextButton(
+                  onPressed: _launchUrl,
+                  child: Text(site.name),
+                ),
                 Row(
                   children: [
                     FiveStarRate(rate: rate),
